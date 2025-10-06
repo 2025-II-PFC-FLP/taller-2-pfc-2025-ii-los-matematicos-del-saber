1df758e296c4177da03a8572f91c1840b788d505
Juan Diego Ospina <juamdiego25@gmail.com>
2025-10-06 16:51:50 -0500

    Correccion

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -6,7 +6,7 @@ class ConjuntosDifusos {
 
   def pertenece(elem: Int, s: ConjDifuso): Double = s(elem)
 
-  // Igualdad autonoma sin depender de inclusion
+
   def igualdad(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
     @annotation.tailrec
     def aux(i: Int): Boolean = {
@@ -20,7 +20,6 @@ class ConjuntosDifusos {
   def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
     import scala.annotation.tailrec
     @tailrec
-    //Implementacion de la funcion
     def aux(i: Int): Boolean = {
       if (i > 1000) true
       else if (cd1(i) > cd2(i)) false
